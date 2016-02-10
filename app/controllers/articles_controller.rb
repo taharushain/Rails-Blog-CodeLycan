499669f6a87d10ea5f5a0871f44f30f31a157b1f
Taha Rushain <taharushain@gmail.com>
2016-02-10 23:24:12 +0500

    article edit and list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -15,6 +20,20 @@ class ArticlesController < ApplicationController
 
   def show 
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])    
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
